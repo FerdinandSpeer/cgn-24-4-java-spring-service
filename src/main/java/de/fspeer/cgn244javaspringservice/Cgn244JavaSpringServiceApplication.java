678b92e14be3d26c1c761d8157676478cbdbe33b
ferdinandspeer <ferdinandspeer@gmail.com>
2024-09-10 14:19:12 +0200

    Initial commit - setup project
    - spring initializer
    - spring web
    - spring mongo data
    - lombok

---
+++ src/main/java/de/fspeer/cgn244javaspringservice/Cgn244JavaSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package de.fspeer.cgn244javaspringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cgn244JavaSpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cgn244JavaSpringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/fspeer/cgn244javaspringservice/Cgn244JavaSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fspeer.cgn244javaspringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cgn244JavaSpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
